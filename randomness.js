3d3a4b9fd1616dc3377ae6f5bae73e108953ee07
justinAguirreASC6 <justinaguirre130@gmail.com>
2019-07-11 12:00:38 -0400

    What's up

---
+++ randomness.js
@@ -0,0 +1,31 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes",
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert",
+];
+
+console.log(foods);
+
+function randomNumber(){
+    const integer = Math.random();
+    console.log(integer*21);
+};
+randomNumber();
+
+function randomLetter(){
+    
+}
+console.log(students[0][1]);
